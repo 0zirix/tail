3d2db816cf8ec8fe48eedd94ff352f7a10dc48f4
Mel Florance <mel.florance@gmail.com>
2022-01-20 03:03:51 +0100

    Update library, make class more consistent. Add option to stop the watcher

--- tail.js
+++ tail.js
@@ -1,59 +1,65 @@
 const fs = require('fs');
-const { watch } = require('fs/promises');
 const Stream = require('stream');
-const readline = require('readline');
 const Events = require('events');
-const abort_controller = new AbortController();
-const { signal } = abort_controller;
+const readline = require('readline');
+const { watch } = require('fs/promises');
 
 module.exports = class Tail extends Events {
     constructor(filepath) {
         super();
-
-        (async () => {
-            try {
-                const watcher = watch(filepath, { signal });
-        
-                for await (const event of watcher) {
-                    switch (event.eventType) {
-                        case 'change': {
-                            try {
-                                let line = await this.getLastLine(event.filename);
-
-                                if (line.length > 0)
-                                    this.emit('line', line);
-                            }
-                            catch (error) {
-                                this.emit('error', error);
-                            }
-                            break;
-                        }
-                    }
-                }
-          
-            } 
-            catch (error) {
-                if (error.name === 'AbortError')
-                    return;
-        
-                throw error;
-            }
-        })();
+        this.controller = new AbortController();
+        this.start(filepath);
     }
 
-    getLastLine(filepath, length = 1) {
-        let input = fs.createReadStream(filepath);
+    stop() {
+        this.controller.abort();
+        this.emit('close');
+    }
+
+    async start(filepath) {
+        try {
+            const watcher = watch(filepath, {
+                signal: this.controller.signal
+            });
+
+            for await (const event of watcher) {
+                switch (event.eventType) {
+                    case 'change': {
+                        try {
+                            let line = await this.getLastLine(event.filename);
+
+                            if (line.length > 0)
+                                this.emit('line', line);
+                        }
+                        catch (error) {
+                            this.emit('error', error);
+                        }
+                        break;
+                    }
+                }
+            }
+
+        }
+        catch (error) {
+            if (error.name === 'AbortError')
+                return;
+
+            throw error;
+        }
+    }
+
+    getLastLine(src, min_line_length = 1) {
+        let input = fs.createReadStream(src);
         let output = new Stream();
-    
+
         return new Promise((resolve, reject) => {
-            let last = '';
-            let rl = readline.createInterface(input, output);
-    
+            let last, rl = readline.createInterface(input, output);
+
             rl.on('line', line => {
-                if (line.length >= length)
+                if (line.length >= min_line_length)
                     last = line;
             });
-    
+
             rl.on('error', reject)
             rl.on('close', () => resolve(last));
         })
--- test.js
+++ test.js
@@ -1,11 +1,28 @@
 const Tail = require('./tail');
+const fs = require('fs');
 
 const stream = new Tail('./test.log');
 
 stream.on('line', line => {
-    console.log('newline:', line, line.length)
+    console.log('newline:', line)
 });
 
 stream.on('error', error => {
     console.log(error);
 });
+
+stream.on('close', () => {  
+    console.log('Stream closed.');
+});
+
+
+let i = 0;
+let interval = setInterval(() => {
+    fs.writeFileSync('./test.log', i.toString() + '\n');
+    ++i;
+}, 1000);
+
+setTimeout(() => {
+    stream.stop();
+    clearInterval(interval);
+}, 4000)
